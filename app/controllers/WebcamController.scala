aff95c0bec4a3ac2dc8b83316ee4866c6e4ee64f
Minh <minhnguyen1995@googlemail.com>
2017-08-24 19:53:00 +0100

    RTC

--- app/controllers/WebcamController.scala
+++ app/controllers/WebcamController.scala
@@ -1,4 +1,5 @@
 package controllers
+import scala.util.parsing.json._
 
 import javax.inject._
 import play.api._
@@ -21,16 +22,60 @@ import akka.stream.OverflowStrategy
 import akka.pattern._
 import play.api.mvc.WebSocket.MessageFlowTransformer
 
+trait RTCsignal {
+  
+}
+case class Offer(data: JsValue) extends RTCsignal
+case class Answer(data: JsValue)  extends RTCsignal
+case class IceCandidate(data: JsValue)  extends RTCsignal
+
+case class WebcamClient(ref: ActorRef)
 
 class WebcamServer extends Actor{
+    println("server created")
+    private var users: List[ActorRef] = List()
+
     def receive: Receive = {
-        case x @ _ => println(x)
+        case WebcamClient(ref) => 
+          users = ref :: users
+        case msg: String => 
+          val jsvalue = Json.parse(msg)
+          val ob = JSON.parseFull(msg)
+          println(msg)
+          println(ob)
+          ob.map(m => m match {
+            case m1: Map[String, String] => (m1 get "type") match {
+              case Some(rtctype) => parseType(rtctype, jsvalue).map(process)
+              case None => val maybeCandidate = (m1 get "candidate").map(_ => parseType("candidate", jsvalue))
+                           maybeCandidate match {
+                             case Some(candidate) => candidate.map(process)
+                             case None => println("ERR")
+                           }
+            }               
+            case _ => "NOPE"
+          })
+    }
+
+    def parseType(rtctype: String, rtc: JsValue): Option[RTCsignal] = rtctype match {
+      case "offer" => Some(Offer(rtc))
+      case "answer" => Some(Answer(rtc))
+      case "candidate" => Some(IceCandidate(rtc))
+      case _    => None
+    }
+    def process(signal: RTCsignal) = signal match {
+      case Offer(data) => users.map(u => u ! Json.stringify(data)); println("sent")
+      case Answer(data) => users.map(u => u ! Json.stringify(data))
+      case IceCandidate(data) => users.map(u => u ! Json.stringify(data)); println("ICE CANDIDATE SENT")
     }
 }
 
+
+
+
+
 @Singleton
 class WebcamController @Inject()(implicit actorSystem: ActorSystem, mat: Materializer) extends Controller{
-  var x = 0
+   
 
   val server = actorSystem.actorOf(Props(new WebcamServer))
 
@@ -40,12 +85,13 @@ class WebcamController @Inject()(implicit actorSystem: ActorSystem, mat: Materia
     Ok(views.html.webcam(url))
   }
 
+
   def socket(id: Int) : WebSocket = WebSocket.accept[String, String] { 
     implicit val transformer = MessageFlowTransformer
      request => 
      val out: Source[String, NotUsed] = Source.actorRef[String](10, OverflowStrategy.fail)
      .mapMaterializedValue{ 
-            outActor => outActor
+            outActor => server ! WebcamClient(outActor)
             NotUsed
      }
      val in = Sink.foreach[Any](
@@ -53,5 +99,4 @@ class WebcamController @Inject()(implicit actorSystem: ActorSystem, mat: Materia
      )
     Flow.fromSinkAndSource(in, out)
   }
-
 }
